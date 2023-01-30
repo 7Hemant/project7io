c9f6df34fe19170319c573d697288ec5767038bd
7hemant <hemantmourya127@gmail.com>
2023-01-31 00:14:19 +0530

    set up

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="grid place-content-center h-screen bg-black text-white text-2xl">
+      Coming soon
+    </div>
+  );
+};
+
+export default Home;
